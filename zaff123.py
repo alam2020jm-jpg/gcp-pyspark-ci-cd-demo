a62961f9979034ea400c491c3cdaa840e04065fd
Zaffir <alam2008jm@gmail.com>
2025-11-30 15:42:35 +0000

    code modified

--- zaff123.py
+++ zaff123.py
@@ -1,4 +1,8 @@
 def xyz(a,b,c):
 	s=a+b+c
-	print(f"Sum of three number={s}")
+	print(f"Sum of three number {a} , {b} and {c}  ={s}")
+	p=a*b*c
+	print(f"Product of three number {a} ,{b} and {c} ={p}")
+x=xyz(4,5,7)
+print(x)
 
